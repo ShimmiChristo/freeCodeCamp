7ec732f39a517a5b0c51be68fa735868f1279970
shimmiChristo <shimmin.christopher@gmail.com>
2018-10-11 22:53:06 -0400

    seek and destroy exercise

--- Javascript_Algorithms_And_Data_Structures_Certification/Intermediate_Algorithm_Scripting/seek_and_destroy.js
+++ Javascript_Algorithms_And_Data_Structures_Certification/Intermediate_Algorithm_Scripting/seek_and_destroy.js
@@ -2,8 +2,44 @@
 // Remove all elements from the initial array that are of the same value as these arguments.
 
 function destroyer(arr) {
-    // Remove all the values
-    return arr;
+  // Remove all the values
+  var newArr = [];
+  for(var i =1; i < arguments.length; i++) {
+    newArr.push(arguments[i]);
   }
-  
-  destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+   return (arr.filter(el => newArr.indexOf(el) === -1 ));
+
+  // return arr;
+}
+
+destroyer([1, 2, 3, 1, 2, 3], 2, 3);
+
+
+
+// BASIC CODE SOLUTION
+
+function destroyer(arr) {
+  var args = Array.prototype.slice.call(arguments);
+
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < args.length; j++) {
+      if (arr[i] === args[j]) {
+        delete arr[i];
+      }
+    }
+  }
+  return arr.filter(Boolean);
+}
+
+
+// INTERMEDIATE CODE SOLUTION
+function destroyer(arr) {
+  var args = Array.from(arguments).slice(1);
+  return arr.filter(function(val) {
+    return !args.includes(val);
+  });
+}
+
+
+// ADVANCED CODE SOLUTION (one line!!)
+const destroyer = (arr, ...args) => arr.filter(i => !args.includes(i));
