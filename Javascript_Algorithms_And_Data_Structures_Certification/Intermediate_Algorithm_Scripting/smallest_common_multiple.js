25ea3c5b26f5aec9ccc111c3c5edc117f20db879
shimmiChristo <shimmin.christopher@gmail.com>
2018-11-07 21:42:26 -0500

    changes

--- Javascript_Algorithms_And_Data_Structures_Certification/Intermediate_Algorithm_Scripting/smallest_common_multiple.js
+++ Javascript_Algorithms_And_Data_Structures_Certification/Intermediate_Algorithm_Scripting/smallest_common_multiple.js
@@ -1,7 +1,28 @@
 function smallestCommons(arr) {
-    console.log(arguments);
-    
+    // console.log(arguments[0]);
+    var newArr = [];
+    var inbetweenNums = [];
+    newArr.push(arr[0],arr[1]);
+    newArr.sort().reverse();
+    var first = newArr[0];
+    var sec = newArr[1];
+    for(var i=sec+1; i<first; i++ ) {
+      newArr.push(i);
+      inbetweenNums.push(i);
+      // console.log(i);  
+    }
+    newArr.sort().reverse();
+  
+  if()
+  
+  
+  
+    // while()
+  
   
     return arr;  
   
   }
+  
+  smallestCommons([1, 5]);
+  smallestCommons([5, 1]);
