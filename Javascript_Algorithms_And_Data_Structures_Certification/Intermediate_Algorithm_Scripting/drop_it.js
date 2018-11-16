87289f4c79f851a7ebe50c7afd1c1d54c5438d11
shimmiChristo <shimmin.christopher@gmail.com>
2018-11-15 23:23:26 -0500

    drop it complete

--- Javascript_Algorithms_And_Data_Structures_Certification/Intermediate_Algorithm_Scripting/drop_it.js
+++ Javascript_Algorithms_And_Data_Structures_Certification/Intermediate_Algorithm_Scripting/drop_it.js
@@ -1,20 +1,37 @@
 function dropElements(arr, func) {
   // Drop them elements.
-  return arr;
-}
+  var newLen = arr.length; // **need variable so value changes in for loop
+  for (var i=0; i < newLen; i++) {
+      if (func(arr[0])) {
+        break;
+      } else {
+          arr.shift();
+      }
+  }
+      console.log(arr);
+      return arr;
+   }
+  
+  dropElements([1, 2, 3], function(n) {return n < 3; }); 
+  dropElements([1, 2, 3, 4], function(n) {return n > 5;})
 
-dropElements([1, 2, 3], function(n) {return n < 3; });
+  
+  //INTERMEDIATE CODE SOULTION
+  function dropElements(arr, func) {
+    return arr.slice(arr.findIndex(func) >= 0 ? arr.findIndex(func): arr.length, arr.length);
+  }
+  
+  // test here
+  dropElements([1, 2, 3, 4], function(n) {return n >= 3;});
 
 
-function dropElements(arr, func) {
-// Drop them elements.
-for (var i =0; i<arr.length; i++) {
-    if (func (i) == false) {
-
-}
-}
-
-return arr;
-}
-
-dropElements([1, 2, 3], function(n) {return n < 3; }); 
+  //ADVANCED CODING SOLUTION
+  function dropElements(arr, func) {
+    while(arr.length > 0 && !func(arr[0])) {
+      arr.shift();
+    }
+    return arr;
+  }
+  
+  // test here
+  dropElements([1, 2, 3, 4], function(n) {return n >= 3;});
