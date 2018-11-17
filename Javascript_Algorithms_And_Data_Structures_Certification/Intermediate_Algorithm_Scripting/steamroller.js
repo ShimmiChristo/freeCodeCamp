96a05b217519ea59deda14df08b7c5baa419e419
shimmiChristo <shimmin.christopher@gmail.com>
2018-11-16 23:54:35 -0500

    steamroller starting project

---
+++ Javascript_Algorithms_And_Data_Structures_Certification/Intermediate_Algorithm_Scripting/steamroller.js
@@ -0,0 +1,20 @@
+// Flatten a nested array. You must account for varying levels of nesting.
+// Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
+function steamrollArray(arr) {
+    // I'm a steamroller, baby
+    // Flatten 1 nested array
+    // Check if array is present in array. 
+    // Use typeof array? No
+    // Use array.includes? 
+    // Use Array.isArray? Yes
+    // Flatten 1 nested array
+    if (Array.isArray(arr)) { 
+      console.log('real array');
+      if(arr.length>0) { 
+  
+      }
+    }
+    return arr;
+  }
+  
+  steamrollArray([1, [2], [3, [[4]]]]);
