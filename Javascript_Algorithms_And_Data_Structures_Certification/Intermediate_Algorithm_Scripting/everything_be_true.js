b3d0c0e8c07477ae40a265f5578865705133ee15
shimmiChristo <shimmin.christopher@gmail.com>
2018-11-17 23:02:31 -0500

    everything be true solution

--- Javascript_Algorithms_And_Data_Structures_Certification/Intermediate_Algorithm_Scripting/everything_be_true.js
+++ Javascript_Algorithms_And_Data_Structures_Certification/Intermediate_Algorithm_Scripting/everything_be_true.js
@@ -3,33 +3,81 @@ function truthCheck(collection, pre) {
     //Only if all of them are truth will we return true, so make sure all of them check.
     
     // Is everyone being true?
-  for(var i=0; i<collection.length; i++) {  
-    var property1 = collection[i];
-    for (var j in property1) {
-      console.log(j);
-      if (!property1.hasOwnProperty(j)){
-        // console.log(collection[i][j]);
-        return false;
-      } 
-      else {
-        return true;
+
+    var findTruth = true;
+    for(var i=0; i<collection.length; i++) {  
+        for (var j in collection[i]) {
+            if (!collection[i].hasOwnProperty(pre) || !collection[i][j]){
+                findTruth = false;
+            } 
+            else {
+                // return true;
+            }
+        }
+    }
+        console.log(findTruth);
+        return findTruth;
+    }
+  
+    truthCheck([
+        {"name": "Pete", "onBoat": true}, 
+        {"name": "Repeat", "onBoat": true}, 
+        {"name": "FastFoward", "onBoat": null}
+    ], "onBoat")
+    
+    // truthCheck([
+    //   {"user": "Tinky-Winky", "sex": "male"}, 
+    //   {"user": "Dipsy", "sex": "male"}, 
+    //   {"user": "Laa-Laa", "sex": "female"}, 
+    //   {"user": "Po", "sex": "female"}
+    // ], "sex"); // should return true
+    
+    // truthCheck([
+    //   {"user": "Tinky-Winky", "sex": "male"}, 
+    //   {"user": "Dipsy"}, 
+    //   {"user": "Laa-Laa", "sex": "female"}, 
+    //   {"user": "Po", "sex": "female"}
+    // ], "sex") //should return false
+
+  
+
+
+//   BASIC CODE SOLUTION
+function truthCheck(collection, pre) {
+    // Create a counter to check how many are true.
+    var counter = 0;
+    // Check for each object
+    for (var c in collection) {
+      // If it is has property and value is truthy
+      if (collection[c].hasOwnProperty(pre) && Boolean(collection[c][pre])) {
+        counter++;
       }
     }
-  }
-    console.log(pre);
-    return pre;
+    // Outside the loop, check to see if we got true for all of them and return true or false
+    return counter == collection.length;
   }
   
-  // truthCheck([
-  //   {"user": "Tinky-Winky", "sex": "male"}, 
-  //   {"user": "Dipsy", "sex": "male"}, 
-  //   {"user": "Laa-Laa", "sex": "female"}, 
-  //   {"user": "Po", "sex": "female"}
-  // ], "sex"); // should return true
+  // test here
+  truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+
+
+
+//INTERMEDIATE CODE SOLUTION
+function truthCheck(collection, pre) {
+    return collection.every(function (element) {
+      return element.hasOwnProperty(pre) && Boolean(element[pre]);
+    });
+  }
   
-  truthCheck([
-    {"user": "Tinky-Winky", "sex": "male"}, 
-    {"user": "Dipsy"}, 
-    {"user": "Laa-Laa", "sex": "female"}, 
-    {"user": "Po", "sex": "female"}
-  ], "sex") //should return false
+  // test here
+  truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
+
+
+
+//ADVANCED CODE SOLUTION
+function truthCheck(collection, pre) {
+    // Is everyone being true?
+    return collection.every(obj => obj[pre]);
+  }
+  
+  truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
