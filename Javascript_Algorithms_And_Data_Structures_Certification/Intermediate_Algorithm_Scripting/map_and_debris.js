c44058aa6d2b6c2f45682b6521058eadb1380e0e
shimmiChristo <shimmin.christopher@gmail.com>
2018-11-27 23:24:51 -0500

    Map and Debris project

--- Javascript_Algorithms_And_Data_Structures_Certification/Intermediate_Algorithm_Scripting/map_and_debris.js
+++ Javascript_Algorithms_And_Data_Structures_Certification/Intermediate_Algorithm_Scripting/map_and_debris.js
@@ -1,7 +1,9 @@
+
 function orbitalPeriod(arr) {
   var GM = 398600.4418;
   var earthRadius = 6367.4447;
   var t = 2 * Math.PI;
+  var newArr = [];
   // notes
   // need to use JS Math Pow
   // need to use Math.sqrt()
@@ -9,9 +11,13 @@ function orbitalPeriod(arr) {
   // var radius = ((earthRadis + avgAlt)^3)/GM;
   var radius = Math.pow(earthRadius + 35873.5553,3)/GM;
   var sqrt = Math.sqrt(radius);
+  var orbitPer = Math.round(t * sqrt);
+  console.log(orbitPer);
 
-  console.log(Math.round(t * sqrt));
-
+  for (var i in arr) {
+      newArr.push(getOrbPeriod(arr[elem]));
+  }
+      console.log(newArr);  
   return arr;
 }
 
