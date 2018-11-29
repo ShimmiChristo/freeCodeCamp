4d5233e05176b886279fc7b466e8ec87cf525f76
shimmiChristo <shimmin.christopher@gmail.com>
2018-11-28 23:43:20 -0500

    I need to change avgAlt to orbitalPeriod

--- Javascript_Algorithms_And_Data_Structures_Certification/Intermediate_Algorithm_Scripting/map_and_debris.js
+++ Javascript_Algorithms_And_Data_Structures_Certification/Intermediate_Algorithm_Scripting/map_and_debris.js
@@ -22,3 +22,52 @@ function orbitalPeriod(arr) {
 }
 
 orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+
+
+
+function orbitalPeriod(arr) {
+  var GM = 398600.4418;
+  var earthRadius = 6367.4447;
+  var firstElem = arr;
+  var newArr = [];
+  // getting orbital period formula
+  // Math.PI 
+  // Math.pow
+  // Math.sqrt
+  // Math.round
+  var t = 2 * Math.PI;
+  var earthRadius = 6367.4447;
+  var GM = 398600.4418;
+
+  for(var i=0; i<arr.length; i++){
+    var newObj = arr[i];
+    for (var elem in newObj){
+        // console.log(elem);
+        // console.log(newObj[elem]);
+        if (elem == 'avgAlt') {
+          // console.log(newObj[elem]);
+          var avgAltitude = newObj[elem];
+          var earthOrb = Math.pow((earthRadius + avgAltitude),3);
+          var solution = t * Math.sqrt(earthOrb/GM);  
+          // console.log(Math.round(solution));
+          newObj[elem] = Math.round(solution);
+          console.log(elem = 'orbitalPeriod');
+          // return (newObj[elem]);
+          // newArr.push(newObj[elem]);
+        }
+    }
+  }
+  for (var j=0;j<arr.length;j++) {
+    // console.log(arr[j]);
+    var doubleX = arr[j];
+    for (var x in doubleX){
+      console.log(x);
+      // console.log(doubleX[x]);
+    }
+  }
+  // console.log(arr);
+  return arr;
+}
+
+// orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}]);
